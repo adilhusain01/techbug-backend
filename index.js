03e3538ea4c74181d6eef2fc32c6caa6af870ad3
adilhusain01 <husainadil202@gmail.com>
2024-07-30 10:53:52 +0530

    .

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ import authRoute from './routes/authRoute.js';
 
 const PORT = 8080 || process.env.PORT;
 
-app.use(cors);
-
 connectDB();
 
 const app = express();
+
+app.use(cors);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
