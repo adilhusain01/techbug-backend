8e0d78f7f49a5d4d2fdbd8c36de13b4b4b4d465b
adilhusain01 <husainadil202@gmail.com>
2024-08-24 17:08:10 +0530

    .

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ['http://localhost:5173', '*'];
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://tech-bug.netlify.app',
+  'https://66c606a3d7e7ad1fa1719c44--tech-bug.netlify.app/',
+];
 
 export default allowedOrigins;
