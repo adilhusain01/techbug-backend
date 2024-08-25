79abefb7e62019d037dab49cc29bae50b6a3fb28
adilhusain01 <husainadil202@gmail.com>
2024-08-25 09:22:13 +0530

    .

--- controllers/userController.js
+++ controllers/userController.js
@@ -116,7 +116,7 @@ export const createUser = async (req, res) => {
 export const updateUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const {
+    let {
       first_name,
       last_name,
       username,
